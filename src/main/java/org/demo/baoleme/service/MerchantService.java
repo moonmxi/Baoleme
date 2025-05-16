01f020d8d2cac8a636bb39184c1ae727c97be1d2
胡淞程 <15522917+buaa23371212@user.noreply.gitee.com>
2025-05-16 13:15:11 +0800

    create MerchantService

---
+++ src/main/java/org/demo/baoleme/service/MerchantService.java
@@ -0,0 +1,19 @@
+package org.demo.baoleme.service;
+
+import org.demo.baoleme.pojo.Merchant;
+
+import java.util.List;
+
+public interface MerchantService {
+    // 创建商户
+    Merchant createMerchant(Merchant merchant);
+    // 根据ID查询商户
+    Merchant getMerchantById(Long id);
+    Merchant getMerchantByUsername(String username);
+    // 查询所有商户
+    List<Merchant> getAllMerchants();
+    // 更新商户信息
+    Merchant updateMerchant(Merchant merchant);
+    // 删除商户
+    void deleteMerchant(Long id);
+}

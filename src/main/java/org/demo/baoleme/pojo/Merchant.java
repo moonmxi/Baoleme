89a9079d7a377f74cf14b1b07aaa2478dddcbfd2
胡淞程 <15522917+buaa23371212@user.noreply.gitee.com>
2025-05-16 13:13:17 +0800

    uodate Merchant

--- src/main/java/org/demo/baoleme/pojo/Merchant.java
+++ src/main/java/org/demo/baoleme/pojo/Merchant.java
@@ -1,24 +1,10 @@
 package org.demo.baoleme.pojo;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
-
-import java.time.LocalDateTime;
+import org.demo.baoleme.common.BaseUser;
 
 @Data
 @TableName("merchant")
-public class Merchant {
-
-    @TableId(type = IdType.AUTO)
-    private Long id;
-
-    private String username;
-
-    private String password;
-
-    private String phone;
-
-    private LocalDateTime createdAt;
+public class Merchant extends BaseUser {
 }

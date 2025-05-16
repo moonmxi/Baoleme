1af8234818c379f0f24e7088d1ba5f447e83bc21
胡淞程 <15522917+buaa23371212@user.noreply.gitee.com>
2025-05-16 12:53:53 +0800

    create pojo/merchant

---
+++ src/main/java/org/demo/baoleme/pojo/Merchant.java
@@ -0,0 +1,24 @@
+package org.demo.baoleme.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@TableName("merchant")
+public class Merchant {
+
+    @TableId(type = IdType.AUTO)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String phone;
+
+    private LocalDateTime createdAt;
+}

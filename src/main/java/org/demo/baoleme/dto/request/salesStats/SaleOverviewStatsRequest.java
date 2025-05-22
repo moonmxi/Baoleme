9054e44efbb9ce5a9cb05720315ee7cd78fd8b42
胡淞程 <15522917+buaa23371212@user.noreply.gitee.com>
2025-05-22 17:58:39 +0800

    优化request of stats

--- src/main/java/org/demo/baoleme/dto/request/salesStats/SaleOverviewStatsRequest.java
+++ src/main/java/org/demo/baoleme/dto/request/salesStats/SaleOverviewStatsRequest.java
@@ -1,12 +1,15 @@
 package org.demo.baoleme.dto.request.salesStats;
 
 import com.baomidou.mybatisplus.annotation.EnumValue;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class SaleOverviewStatsRequest {
+    @NotBlank(message = "店铺id不能为空")
     private Long storeId;
-    private TimeRange timeRange;
+
+    private TimeRange timeRange = TimeRange.TODAY;
 
     public enum TimeRange {
         TODAY(0),

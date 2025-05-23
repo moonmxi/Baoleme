9cb85b61d26be09942a46d865050f32a32038921
胡淞程 <15522917+buaa23371212@user.noreply.gitee.com>
2025-05-23 21:54:43 +0800

    update

--- src/main/java/org/demo/baoleme/dto/request/merchant/MerchantRegisterRequest.java
+++ src/main/java/org/demo/baoleme/dto/request/merchant/MerchantRegisterRequest.java
@@ -7,6 +7,7 @@ import lombok.Data;
 
 @Data
 public class MerchantRegisterRequest {
+    @NotBlank(message = "用户名不能为空")
     @Size(min = 2, max = 20, message = "用户名长度应为2~20个字符")
     private String username;
 

1c78ebccdd230718387675c78bd325b1eda4c46d
胡淞程 <15522917+buaa23371212@user.noreply.gitee.com>
2025-06-02 00:25:16 +0800

    update

--- src/main/java/org/demo/baoleme/dto/response/store/StoreViewInfoResponse.java
+++ src/main/java/org/demo/baoleme/dto/response/store/StoreViewInfoResponse.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
 @Data
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class StoreViewInfoResponse {
+    private Long storeId;
     private String name;
     private String description;
     private String type;

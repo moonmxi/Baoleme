51861a4830cd138a50175fb90459efbc9369543a
韦秋宇 <23371102>
2025-06-06 21:55:48 +0800

    sql更新，加入了两个接口，用于浏览历史记录

--- src/main/java/org/demo/baoleme/dto/request/user/UserOrderHistoryRequest.java
+++ src/main/java/org/demo/baoleme/dto/request/user/UserOrderHistoryRequest.java
@@ -11,9 +11,7 @@ import java.time.LocalDateTime;
 @Data
 public class UserOrderHistoryRequest {
     private Integer status;
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime startTime;
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime endTime;
     private Integer page;
     private Integer pageSize;
